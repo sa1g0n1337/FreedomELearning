1acfe51ae7b5211fd873009387acbe710783874d
Dreamer112 <36270685+Dreamer112@users.noreply.github.com>
2019-05-13 08:13:33 +0700

    about us

---
+++ app/src/main/java/freedom/com/freedom_e_learning/AboutUsActivity.java
@@ -0,0 +1,25 @@
+package freedom.com.freedom_e_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
