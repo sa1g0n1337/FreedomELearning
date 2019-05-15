8c60e4d1bb72de8fb6de8706da17a87334a32a0e
Dreamer112 <36270685+Dreamer112@users.noreply.github.com>
2019-05-16 00:03:38 +0700

    About Us done

--- app/src/main/java/freedom/com/freedom_e_learning/AboutUsActivity.java
+++ app/src/main/java/freedom/com/freedom_e_learning/AboutUsActivity.java
@@ -3,19 +3,47 @@ package freedom.com.freedom_e_learning;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.GridLayout;
+import android.view.View;
+import android.support.v7.widget.CardView;
+import android.widget.Toast;
+
 
 public class AboutUsActivity extends AppCompatActivity {
     private Toolbar AUtoolbar;
+    GridLayout grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        setControl();
+        gridclick(grid);
+    }
+
+    private void setControl(){
         AUtoolbar = findViewById(R.id.AboutUsToolbar);
         AUtoolbar.setTitle(String.format("About Us"));
         setSupportActionBar(AUtoolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        grid = (GridLayout) findViewById(R.id.grid);
+    }
+
+    private void gridclick(GridLayout grid){
+        for(int i = 0; i<grid.getChildCount(); i++){
+            CardView cardView = (CardView)grid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(AboutUsActivity.this,"clicked at index" + finalI,Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
     }
 
     @Override
